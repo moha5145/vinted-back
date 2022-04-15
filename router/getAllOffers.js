f5fd24f1cdf2d20344673570f7fc42ec30792999
moha5145 <mms.jimma@gmail.com>
2022-04-16 00:17:56 +0200

    update filters

--- router/getAllOffers.js
+++ router/getAllOffers.js
@@ -5,81 +5,8 @@ const { all } = require("./user");
 
 router.get("/offers", async (req, res) => {
   try {
-    // if ( req.query.page || req.query.title || req.query.priceMin || req.query.priceMin || req.query.sort {
-    //   if (req.query.page) {
-    //     const limitByPage = 2;
-    //     if (req.query.page == 1) {
-    // const offerByPage = await Offer.find().limit(limitByPage).skip(0);
-    //       const count = offerByPage.length;
-    //       res.json({ count, offerByPage });
-    //     } else if (req.query.page == 2) {
-    //       const offerByPage = await Offer.find()
-    //         .limit(limitByPage)
-    //         .skip(limitByPage);
-    //       const count = offerByPage.length;
-    //       res.json({ count, offerByPage });
-    //     } else if (req.query.page > 2) {
-    //       const offerByPage = await Offer.find()
-    //         .limit(limitByPage)
-    //         .skip(limitByPage * req.query.page);
-    //       const count = offerByPage.length;
-    //       res.json({ count, offerByPage });
-    //     }
-    //   } else if (req.query.sort === "price-asc" && req.query.title) {
-    //     console.log("hi");
-    //     const findByTitleAndSort = await Offer.find({
-    //       product_name: RegExp(req.query.title, "i"),
-    //     }).sort({ product_price: "asc" });
-
-    //     const count = findByTitleAndSort.length;
-    //     res.json({ count, findByTitleAndSort });
-    //   } else if (req.query.title) {
-    //     const findOffersByTitle = await Offer.find({
-    //       product_name: RegExp(req.query.title, "i"),
-    //     });
-    //     const count = findOffersByTitle.length;
-    //     res.json({ count, findOffersByTitle });
-    //   } else if (req.query.priceMin && req.query.priceMax) {
-    //     const findOffersByPrice = await Offer.find({
-    //       product_price: { $gte: req.query.priceMin, $lte: req.query.priceMax },
-    //     });
-    //     findOffersByPrice;
-    //     const count = findOffersByPrice.length;
-    //     res.json({ count, findOffersByPrice });
-    //   } else if (req.query.title && req.query.priceMax) {
-    //     const findOffersByPrice = await Offer.find({
-    //       product_name: new RegExp(req.query.title, "i"),
-    //       product_price: { $lte: req.query.priceMax },
-    //     });
-    //     const count = findOffersByPrice.length;
-    //     res.json({ count, findOffersByPrice });
-    //   } else if (req.query.sort) {
-    //     if (req.query.sort === "price-desc") {
-    //       const findByDescPrice = await Offer.find().sort({
-    //         product_price: "desc",
-    //       });
-    //       const count = findByDescPrice.length;
-    //       res.json({ count, findByDescPrice });
-    //     } else if (req.query.sort === "price-asc") {
-    //       const findByAscPrice = await Offer.find().sort({
-    //         product_price: "asc",
-    //       });
-    //       const count = findByAscPrice.length;
-    //       res.json({ count, findByAscPrice });
-    //     }
-    //   } else {
-    //     const allOffers = await Offer.find();
-
-    //     res.json(allOffers);
-    //   }
-    // }
-
-    // const allOffers = await Offer.find();
-
     let filter = {};
 
-    req.query.page == 1 ? await Offer.find(filter).limit(2).skip(0) : null;
-    // filter by title
     req.query.title ? (filter.product_name = new RegExp(req.query.title, "i")) : null;
 
     // filter by title and price max
@@ -87,20 +14,36 @@ router.get("/offers", async (req, res) => {
       ? ((filter.product_name = new RegExp(req.query.title, "i")), (filter.product_price = { $lte: req.query.priceMax }))
       : null;
 
-    console.log(filter);
-    // filter by price max and price min
-    req.query.priceMin && req.query.priceMax ? (filter.product_price = { $gte: req.query.priceMin, $lte: req.query.priceMax }) : null;
-
-    // filter by price max only
-    req.query.priceMax ? (filter.product_price = { $lte: req.query.priceMax }) : null;
-
     //filter by price min only
     req.query.priceMin ? (filter.product_price = { $gte: req.query.priceMin }) : null;
 
-    // filter by price asc
+    // filter by price max and price min
+    if (req.query.priceMax) {
+      if (filter.product_price) {
+        filter.product_price.$lte = req.query.priceMax;
+      } else {
+        filter.product_price = { $lte: req.query.priceMax };
+      }
+    }
+
+    // filter by price asc and desc
+    let sort = {};
+    if (req.query.sort === "price-desc") {
+      sort.product_price = "desc";
+    } else if (req.query.sort === "price-asc") {
+      sort.product_price = "asc";
+    }
+
+    let offerLimitParPage = req.query.limit ? req.query.limit : 4;
+
+    let page = req.query.page < 1 ? 1 : req.query.page;
 
     const count = await Offer.countDocuments(filter);
-    const result = await Offer.find(filter);
+    const result = await Offer.find(filter)
+      .select("product_name product_price")
+      .sort(sort)
+      .limit(offerLimitParPage)
+      .skip((page - 1) * offerLimitParPage);
     res.status(400).json({ count, result });
   } catch (error) {
     res.status(400).json({ error: error.message });
