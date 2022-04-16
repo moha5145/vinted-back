7657e2753b07643ec7793afd517902619a032234
moha5145 <mms.jimma@gmail.com>
2022-04-16 14:43:58 +0200

    update delete route

--- router/offerDelete.js
+++ router/offerDelete.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+
 const Offer = require("../models/Offer");
 const isAuthenticated = require("../middleware/isAuthenticated");
-const { application } = require("express");
-const cloudinary = require("cloudinary").v2;
 
+// delete Offer by id
 router.delete("/offer/delete", isAuthenticated, async (req, res) => {
   try {
-    res.json("HELLO");
+    const deletOffer = await Offer.findByIdAndDelete(req.fields.id);
+    if (deletOffer === null) {
+      res.status(400).json({ error: "Please enter valid Id" });
+    } else {
+      res.json({ message: "Offer successfully deleted", offer: deletOffer });
+    }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
